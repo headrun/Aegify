61b18b7a9733f0cf02c83018cd7373571e69aced
farooqbabajan1540 <farooq@headrun.com>
2021-02-18 17:28:50 +0530

    removing junk files

--- base/celery_app/celeryconfig.py
+++
@@ -1,4 +0,0 @@
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ROUTES = {'task_name': {'queue': 'queue_name_for_task'}}
-CELERY_IMPORTS = ('crawl.celery.tasks',)
